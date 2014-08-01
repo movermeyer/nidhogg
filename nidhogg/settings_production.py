b40801db9f17ddcefc0a6f628d000943ef4e1721
Orhideous <orhideous@gmail.com>
2014-08-02 00:52:09 +0300

    Rearranged settings

--- nidhogg/settings_production.py
+++ nidhogg/settings_production.py
@@ -1,3 +1,12 @@
 from settings import *
 
 DEBUG = False
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}".format(
+    **{
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+)
--- nidhogg/settings_test.py
+++ nidhogg/settings_test.py
@@ -1,7 +1,8 @@
-DEBUG = True
-TESTING = False
-SECRET_KEY = 'qh\x98\xc4o\xc4]\x8f\x8d\x93\xa4\xec\xc5\xfd]\xf8\xb1c\x84\x86\xa7A\xcb\xc0'
-CURRENT_CMS = 'wordpress'
+from settings import *
+
+DEBUG = False
+TESTING = True
+
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}".format(
     **{
         'NAME': 'test',
