031fe8bdddbc31ed66b56473485ba9f0b53a6f3c
Orhideous <orhideous@gmail.com>
2014-08-08 01:51:34 +0300

    Connect custom error handler to app

--- nidhogg/application.py
+++ nidhogg/application.py
@@ -1,21 +1,24 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 from flask import Flask
+
+from protocol.exceptions import YggdrasilError
 
 
 def create_app(config_filename):
-
     application = Flask(__name__)
     application.config.from_object(config_filename)
 
     from common.database import db
+
     db.init_app(application)
 
     from pages.views import pages_app
     from ajax.views import ajax_app
-    from protocol.views import protocol_app
+    from protocol.views import protocol_app, error_handler
 
     application.register_blueprint(pages_app, url_prefix='/admin')
     application.register_blueprint(ajax_app, url_prefix='/ajax')
     application.register_blueprint(protocol_app)
+    application.register_error_handler(YggdrasilError, error_handler)
 
     return application
