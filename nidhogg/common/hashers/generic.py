74dc3e0973f648cdd0526a12562f427544c12f62
Orhideous <orhideous@gmail.com>
2014-08-09 18:11:04 +0300

    Add set_password method for User model

---
+++ nidhogg/common/hashers/generic.py
@@ -0,0 +1,6 @@
+def check_password(raw=None, hash=None):
+    return True
+
+
+def set_password(raw=None):
+    return 'qwerty'
--- nidhogg/common/models.py
+++ nidhogg/common/models.py
@@ -29,6 +29,9 @@ class User(db.Model):
     def check_password(self, raw_password):
         return hasher.check_password(raw=raw_password, hash=self.password)
 
+    def set_password(self, raw_password):
+        self.password = hasher.make_password(raw=raw_password)
+
 
 class Token(db.Model):
     __tablename__ = 'minecraft_tokens'
