770f8b63096f6ce2dd897759e14834db6213fc64
Orhideous <orhideous@gmail.com>
2014-08-11 11:25:59 +0300

    Implement signout and invalidate logic. Refactored base class.

--- nidhogg/protocol/request.py
+++ nidhogg/protocol/request.py
@@ -2,6 +2,7 @@ import uuid
 import json
 
 from sqlalchemy import or_
+from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
 from common.database import db
@@ -68,12 +69,106 @@ class Request:
 
         return uuid.uuid1().hex
 
+    @staticmethod
+    def validate_credentials(payload):
+        """Validates credentials format
+
+        :type payload: dict
+        :param payload: Dict containing username and password
+        :raise exc.EmptyCredentials:
+        :raise exc.InvalidCredentials:
+        """
+
+        username = payload.get("username")
+        password = payload.get("password")
+
+        try:
+            assert username is not None and password is not None
+        except AssertionError:
+            raise exc.EmptyCredentials
+
+        try:
+            assert isinstance(username, str)
+            assert isinstance(password, str)
+        except AssertionError:
+            raise exc.InvalidCredentials
+
+    @staticmethod
+    def get_user(username=None, password=None):
+        """Get user object from database.
+
+        :type username: str
+        :param username: Username
+        :type password: str
+        :param password: Password
+        :return: User instance
+        :rtype: User
+        :raise exc.InvalidCredentials:
+        :raise exc.MigrationDone:
+        """
+        try:
+            user = (
+                User.query
+                .options(joinedload('token'))
+                .filter(or_(User.login == username, User.email == username))
+                .one()
+            )
+        except (NoResultFound, MultipleResultsFound):
+            raise exc.InvalidCredentials
+
+        if not user.check_password(raw_password=password):
+            raise exc.InvalidCredentials
+
+        if username == user.login:
+            raise exc.MigrationDone
+
+        return user
+
+    @staticmethod
+    def validate_token_pair(payload):
+        """Validates tokens format
+
+        :type payload: dict
+        :param payload: Dict containing accessToken and clientToken
+        :raise exc.InvalidToken:
+        """
+        client_token = payload.get("clientToken")
+        access_token = payload.get("accessToken")
+        try:
+            assert isinstance(client_token, str)
+            assert isinstance(access_token, str)
+            assert len(client_token)
+            assert len(access_token)
+        except AssertionError:
+            raise exc.InvalidToken
+
+    @staticmethod
+    def get_token(client_token_value):
+        """Fetches Token object by clientToken string
+
+        :param client_token_value: clientToken value
+        :type client_token_value: str
+        :return: Token instance
+        :rtype: Token
+        :raise exc.InvalidToken:
+        """
+        try:
+            token = (
+                Token.query
+                .filter(Token.client == client_token_value)
+            ).one()
+        except (NoResultFound, MultipleResultsFound):
+            raise exc.InvalidToken
+        else:
+            return token
+
 
 class Authenticate(Request):
     """Yggdrasil authentication request."""
 
     def validate(self, payload):
         super().validate(payload)
+        self.validate_credentials(payload)
 
         agent = payload.get("agent")
         if agent is not None:
@@ -90,20 +185,6 @@ class Authenticate(Request):
             except AssertionError:
                 raise exc.BadPayload
 
-        username = payload.get("username")
-        password = payload.get("password")
-
-        try:
-            assert username is not None and password is not None
-        except AssertionError:
-            raise exc.EmptyCredentials
-
-        try:
-            assert isinstance(username, str)
-            assert isinstance(password, str)
-        except AssertionError:
-            raise exc.InvalidCredentials
-
     def process(self):
         """Authenticates a user using his password.
 
@@ -116,25 +197,12 @@ class Authenticate(Request):
             This will however also invalidate all previously acquired
             accessTokens for this user across all clients.
         """
-        username = self.payload.get("username")
-        password = self.payload.get("password")
 
-        try:
-            user = (
-                User.query
-                .filter(or_(User.login == username, User.email == username))
-                .one()
-            )
-        except (NoResultFound, MultipleResultsFound):
-            raise exc.InvalidCredentials
-
-        if not user.check_password(raw_password=password):
-            raise exc.InvalidCredentials
-
-        if username == user.login:
-            raise exc.MigrationDone
-
-        token = Token()
+        user = self.get_user(
+            username=self.payload.get("username"),
+            password=self.payload.get("password")
+        )
+        token = user.token or Token()
         token.access = self._generate_token()
         token.client = self.payload.get("clientToken", self._generate_token())
         user.token = token
@@ -155,15 +223,7 @@ class Refresh(Request):
     """Yggdrasil refresh request."""
 
     def validate(self, payload):
-        client_token = payload.get("clientToken")
-        access_token = payload.get("accessToken")
-        try:
-            assert isinstance(client_token, str)
-            assert isinstance(access_token, str)
-            assert len(client_token)
-            assert len(access_token)
-        except AssertionError:
-            raise exc.InvalidToken
+        self.validate_token_pair(payload)
 
     def process(self):
         """Refreshes a valid accessToken.
@@ -172,15 +232,7 @@ class Refresh(Request):
         and is preferred over storing the user's password in a file.
         """
 
-        client_token_value = self.payload.get("clientToken")
-        try:
-            token = (
-                Token.query
-                .filter(Token.client == client_token_value)
-            ).one()
-        except (NoResultFound, MultipleResultsFound):
-            raise exc.InvalidToken
-
+        token = self.get_token(self.payload.get("clientToken"))
         token.access = Request._generate_token()
         db.session.commit()
 
@@ -191,6 +243,8 @@ class Refresh(Request):
 
 
 class Validate(Request):
+    """Yggdrasil validation request"""
+
     def validate(self, payload):
         try:
             access_token = payload.get("accessToken")
@@ -225,10 +279,29 @@ class Validate(Request):
 
 
 class Signout(Request):
+    """Yggdrasil sign out request"""
+
+    def validate(self, payload):
+        self.validate_credentials(payload)
+
     def process(self):
         """Invalidates accessTokens using an account's username and password."""
+        user = self.get_user(
+            username=self.payload.get("username"),
+            password=self.payload.get("password")
+        )
+        db.session.delete(user.token)
+        db.session.commit()
 
 
 class Invalidate(Request):
+
+    def validate(self, payload):
+        self.validate_token_pair(payload)
+
     def process(self):
         """Invalidates accessTokens using a client/access token pair."""
+
+        token = self.get_token(self.payload.get("clientToken"))
+        db.session.delete(token)
+        db.session.commit()
