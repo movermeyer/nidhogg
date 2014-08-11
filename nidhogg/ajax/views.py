d83c9d83d6a3dab9b584e6ca11cacc4960e2db48
Orhideous <orhideous@gmail.com>
2014-08-11 04:50:16 +0300

    Token as container for accessToken+clientToken

--- nidhogg/ajax/views.py
+++ nidhogg/ajax/views.py
@@ -11,5 +11,10 @@ ajax_app = Blueprint('ajax_app', __name__)
 @ajax_app.route('/tokens')
 @json_response
 def tokens():
-    query = db.session.query(User.id, User.login, User.email, Token.value).outerjoin(Token)
+    query = db.session.query(
+        User.id,
+        User.login,
+        User.email,
+        Token.created
+    ).outerjoin(Token)
     return [row._asdict() for row in query]
--- nidhogg/common/models.py
+++ nidhogg/common/models.py
@@ -4,7 +4,7 @@ from importlib import import_module
 
 from sqlalchemy.orm import relationship, backref
 
-from settings import CURRENT_CMS
+from settings.main import CURRENT_CMS
 from common.database import db
 
 
@@ -24,7 +24,11 @@ class User(db.Model):
     password = db.Column(cms_config['password'], db.String(255))
 
     def __repr__(self):
-        return '<{0}: [{1}] {2}>'.format(self.__class__.__name__, self.id, self.login)
+        return '<{0}: [{1}] {2}>'.format(
+            self.__class__.__name__,
+            self.id,
+            self.login
+        )
 
     def check_password(self, raw_password):
         return hasher.check_password(raw=raw_password, hash=self.password)
@@ -37,6 +41,16 @@ class Token(db.Model):
     __tablename__ = 'minecraft_tokens'
 
     id = db.Column(db.Integer, primary_key=True)
-    value = db.Column(db.String(36), nullable=True, unique=True)
-    user = relationship('User', uselist=False, backref=backref("token", uselist=False))
-    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    access = db.Column(db.String(32), nullable=True)
+    client = db.Column(db.String(32), nullable=True)
+    created = db.Column(
+        db.TIMESTAMP,
+        server_default=db.func.now(),
+        onupdate=db.func.current_timestamp()
+    )
+    user = relationship(
+        'User',
+        uselist=False,
+        backref=backref("token", uselist=False)
+    )
+    user_id = db.Column(db.ForeignKey(User.id))
